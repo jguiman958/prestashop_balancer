74df153d53d6d667995ffda09e104197c113ea15
Ubuntu <ubuntu@ip-172-31-84-51.ec2.internal>
2023-12-11 19:27:02 +0000

    ---

--- script/deploy_backend.sh
+++ script/deploy_backend.sh
@@ -8,11 +8,11 @@ set -ex
 source .env
 
 # Creamos la base de datos.
-mysql -u root <<< "DROP DATABASE IF EXISTS $WORDPRESS_DB_NAME"
-mysql -u root <<< "CREATE DATABASE $WORDPRESS_DB_NAME"
-mysql -u root <<< "DROP USER IF EXISTS $WORDPRESS_DB_USER@'$IP_CLIENTE_MYSQL'"
-mysql -u root <<< "CREATE USER $WORDPRESS_DB_USER@'$IP_CLIENTE_MYSQL' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD'"
-mysql -u root <<< "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@'$IP_CLIENTE_MYSQL'"
+mysql -u root <<< "DROP DATABASE IF EXISTS $PS_DB_NAME"
+mysql -u root <<< "CREATE DATABASE $PS_DB_NAME"
+mysql -u root <<< "DROP USER IF EXISTS $PS_DB_USER@$IP_CLIENTE_MYSQL"
+mysql -u root <<< "CREATE USER $PS_DB_USER@$IP_CLIENTE_MYSQL IDENTIFIED BY '$PS_DB_PASSWORD'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $PS_DB_NAME.* TO $PS_DB_USER@$IP_CLIENTE_MYSQL"
 
 # Reiniciamos mysql.
 systemctl restart mysql
