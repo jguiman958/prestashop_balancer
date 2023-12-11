79a73e9697489a7982d455229ec48bd55385c45b
Ubuntu <ubuntu@ip-172-31-83-218.ec2.internal>
2023-12-11 19:21:13 +0000

    ---

--- script/deploy_fronted.sh
+++ script/deploy_fronted.sh
@@ -9,13 +9,6 @@ apt update
 #Incluimos las variables env
 source .env
 
-#Creamos la base de datos y el usuario para prestashop
-mysql -u root <<< "DROP DATABASE IF EXISTS $PS_DB_NAME"
-mysql -u root <<< "CREATE DATABASE $PS_DB_NAME"
-mysql -u root <<< "DROP USER IF EXISTS $PS_DB_USER@$IP_CLIENTE_MYSQL"
-mysql -u root <<< "CREATE USER $PS_DB_USER@$IP_CLIENTE_MYSQL IDENTIFIED BY '$PS_DB_PASSWORD'"
-mysql -u root <<< "GRANT ALL PRIVILEGES ON $PS_DB_NAME.* TO $PS_DB_USER@$IP_CLIENTE_MYSQL"
-
 #Borramos descargas previas
 rm -rf /tmp/prestashop_8.1.2.zip
 
